878643361ae0fe9e1fb4623f90febcf2d07d8016
Jonathan Goldman <me@jonnii.com>
2012-02-20 15:59:15 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Populizer::Application.config.secret_token = '325dd3965e35acc3a980b369bb5c1476aefacb8b02718e1f361e124b427763f9a61341c5e4d2fc338e8adbc40770d7ba429bfa5768e92f9354ade5c496d0b742'
